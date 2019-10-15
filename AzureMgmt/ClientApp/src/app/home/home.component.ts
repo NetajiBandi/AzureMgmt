4ab9c1498dfc659fc39d08b358f813fbf2dccf32
Netaji Bandi <netaji@outlook.com>
2019-10-16 00:50:18 +0530

    1. Deployment endpoint error fix 2. Removed default WeatherForecasts api 3. UI for creating VM from Angular

--- AzureMgmt/ClientApp/src/app/home/home.component.ts
+++ AzureMgmt/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  private _http: HttpClient;
+  private _baseUrl: string;
+
+  public vmConfig: VMConfig = new VMConfig();
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    this._http = http;
+    this._baseUrl = baseUrl;
+  }
+
+  onSubmit() {
+    console.log(this.vmConfig);
+
+    this._http.post(this._baseUrl + 'api/AzureMgmt/CreateVM', this.vmConfig).subscribe(
+      (val) => {
+        console.log("POST call successful value returned in body",
+          val);
+      },
+      response => {
+        console.log("POST call in error", response);
+      },
+      () => {
+        console.log("The POST observable is now completed.");
+      });
+  }
+}
+
+class VMConfig {
+  name: string;
+  size: string;
 }
---
+++ AzureMgmt/Models/VMConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureMgmt.Models
+{
+    public class VMConfig
+    {
+        public string Name { get; set; }
+        public string Size { get; set; }
+    }
+}
